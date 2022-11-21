16116f1ca19facbc184d3f44994b43ebf6848723
Cheef31 <waleed_ahmed@hotmail.de>
2022-11-21 01:37:01 +0100

    Loaded classes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+# Videoquelle
 def load_capture():
     capture = cv2.VideoCapture("rtsp://192.168.0.98/live0")
     return capture
@@ -12,6 +13,13 @@ calcedFPS = 1/sPF
 # Um Framenummer zu loggen
 total_frames = 0
 
+# AI Model laden
+net = cv2.dnn.readNetFromONNX("yolov5n.onnx")
+
+# Classes laden
+classes_file = open("classes.txt", "r")
+classes = classes_file.read().split('\n')
+
 while True:
     # Bild (Video) (eig sogar einzelnes Frame) laden
     capture = load_capture()
@@ -21,6 +29,11 @@ while True:
     if img is None:
         print("End of stream")
         break
+
+    # Für die Anzeige croppen
+    img = cv2.resize(img, (1000, 600))
+
+    # Todo: Weitermachen  bei 09:20 min
 
     # Bild in neuem Fenster namens "Videostream" anzeigen
     cv2.imshow("Videostream", img)
@@ -36,7 +49,7 @@ while True:
     # 1s --> damit fps runter geht --> geringere CPU Auslastung (alter Laptop von 90% auf 30%)
     if cv2.waitKey(sPF*1000) > -1:
         print("-----")
-        print("Script durch Tasteneingabe beendet")
+        print("Script nach Frame #" + str(total_frames) + " durch Tasteneingabe beendet")
         break
 
     # Framenummer loggen
